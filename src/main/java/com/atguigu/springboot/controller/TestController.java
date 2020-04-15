9cbc4fd43567d6d6bae06c4ece7fffa276626e2b
gj6956 <gj6956@163.com>
2020-04-15 22:56:33 +0800

    git 第三次提交

---
+++ src/main/java/com/atguigu/springboot/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.atguigu.springboot.controller;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestController {
+    public static void main(String[] args) {
+        String s1 = "programming";
+        String s2 = new String("programming");
+        String s3 = "program" + "ming";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s2.intern());
+    }
+
+}
